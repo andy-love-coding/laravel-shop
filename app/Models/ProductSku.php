c81a4c33afba16a498af0de450efaa45e0505883
andy <haha@qq.com>
2020-08-14 03:30:45 +0000

    5.1 商品模型

---
+++ app/Models/ProductSku.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    public function product()
+    {
+        $this->belongsTo(Product::class);
+    }
+}
