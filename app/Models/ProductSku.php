ec5e968a0ac32c4c2c97c53369a37676cc18d57b
andy <haha@qq.com>
2020-08-17 05:04:20 +0000

    6.4 订单创建

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -12,4 +12,28 @@ class ProductSku extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        // 执行：$sku->decreaseStock(8)，其对应sql如下：
+        // update `product_skus` set `stock` = `stock` - 8, `product_skus`.`updated_at` = '2020-08-17 04:38:15' where `id` = 95 and `stock` >= '8'
+        // 注意：decrement() 方法要加 ->where('id', $this->id)，如果不加的话，会减去所有 `stock` >= '8' 的库存，对应的sql如下：
+        // update `product_skus` set `stock` = `stock` - 8, `product_skus`.`updated_at` = '2020-08-17 04:38:15' where stock` >= '8'
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        // 执行：$sku->addStock(3)，其对应sql如下：
+        // update `product_skus` set `stock` = `stock` + 3, `product_skus`.`updated_at` = '2020-08-17 04:51:28' where `id` = 95
+        // 注意：increment() 方法不用加 ->where('id', $this->id)，实例执行时会自动加上 where `id` = 95
+        $this->increment('stock', $amount);
+    }
 }
