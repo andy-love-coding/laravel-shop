f19d1d4d739624d89c400d757c5c5337b944080a
andy <haha@qq.com>
2020-08-19 12:29:37 +0000

    8.4 用户界面 - 确认收货

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
   Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
   Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+  
+  Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 });
-// Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
