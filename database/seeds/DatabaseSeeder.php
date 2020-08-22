3fb2f1ca1f3d6d41db7b3398f223db25a7fc7c63
andy <haha@qq.com>
2020-08-22 07:46:31 +0000

    9.2 9.3 填充假数据 备份后台管理数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(AdminTablesSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(UserAddressesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(CouponCodesSeeder::class);
+        $this->call(OrdersSeeder::class);
     }
 }
