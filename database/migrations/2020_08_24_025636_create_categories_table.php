bcc66fd728ba7e24b3d8682c3ccbe6b41bbf9595
andy <haha@qq.com>
2020-08-24 05:06:07 +0000

    3.2 类目 Seeder 文件

--- database/migrations/2020_08_24_025636_create_categories_table.php
+++ database/migrations/2020_08_24_025636_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->unsignedBigInteger('parent_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
             $table->boolean('is_directory');
             $table->unsignedInteger('level');
