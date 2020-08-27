7b642d7610fbaea8d1e920768431029b2ab33429
andy <haha@qq.com>
2020-08-27 06:29:49 +0000

    4.7 测试支付 开发环境内网穿透 Ngrok

--- config/app.php
+++ config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'ngrok_url' => env('NGROK_URL'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
