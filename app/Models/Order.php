4db097fb8ccaa079992cda3488bb6ed5ee48498b
andy <haha@qq.com>
2020-08-27 16:03:06 +0000

    5.2 分期付款数据库结构设计

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -104,7 +104,7 @@ class Order extends Model
         do {
             // Uuid 类可以用来生成大概率不重复的字符串
             $no = Uuid::uuid4()->getHex();
-            // 为了避免重复我们在生成之后再数据库中查询看看是否已经存在相同的退款订单号            
+            // 为了避免重复我们在生成之后再数据库中查询看看是否已经存在相同的退款订单号     
         } while (self::query()->where('refund_no', $no)->exists());
 
         return $no;
